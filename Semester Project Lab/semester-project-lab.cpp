7a526fb956180d86dae8500dc6194fec372d9fd5
Alexa Wu <porkson12@gmail.com>
2025-04-24 16:37:32 -0700

    Add project files.

---
+++ Semester Project Lab/semester-project-lab.cpp
@@ -0,0 +1,32 @@
+// Programmer: Alexa Wu
+// Programmer's ID: 1234567
+
+/*
+Alexa Wu
+
+Lab: Semester Project Lab
+
+4/24/2025
+
+Windows 11 Home, 24H2, ISO C++14 Standard
+
+Description: This program solves the problem of revtriving the quanity stocked and price of an item in storage.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //add staring list of items currently in storage
+    
+    //add user input for what item to find
+    string item;
+    cout << "Input Item Name: ";
+    cin >> item;
+
+    //print out quanity of item and price of item
+    //allow adding and deleting quanity of items and editing price.
+}
+
