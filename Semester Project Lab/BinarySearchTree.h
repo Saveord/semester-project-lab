17366489f91204f627c69db17292b5a579b838bb
Alexa Wu <porkson12@gmail.com>
2025-04-27 00:33:02 -0700

    added delete function and edit function

--- Semester Project Lab/BinarySearchTree.h
+++ Semester Project Lab/BinarySearchTree.h
@@ -13,9 +13,9 @@ public:
 
 	BinarySearchTree()
 	{
-		root = nullptr;
+		root = nullptr; //initalize root as null
 	}
-    //Insert fuction to add new items to binary tree
+    //insert fuction to add new items to binary tree
 	void Insert(string itemName, int quantity, double price)
 	{
         Node* item = new Node(itemName, quantity, price); //creates new node for item
@@ -30,24 +30,24 @@ public:
 
             while (currentNode) 
             {
-                //Compares if item name is alphabetically less than current node's item name
+                //compares if item name is alphabetically less than current node's item name
                 if (item->itemName.compare(currentNode->itemName) < 0) 
                 {
-                    //If item is less than current node then 
+                    //if item is less than current node then 
                     //assign it to the current node's left child
                     if (currentNode->left == nullptr) 
                     {
                         currentNode->left = item;
                         currentNode = nullptr;
                     }
-                    //If current node already has a left child then 
+                    //if current node already has a left child then 
                     //while loop repeats again w/ current node's left child
                     else 
                     {
                         currentNode = currentNode->left;
                     }
                 }
-                //If item is greater than current node then 
+                //if item is greater than current node then 
                 //assign it to the current node's right child
                 else 
                 {
@@ -56,7 +56,7 @@ public:
                         currentNode->right = item;
                         currentNode = nullptr;
                     }
-                    //If current node already has a right child then
+                    //if current node already has a right child then
                     //while loop repeats again w/ current node's right child
                     else
                     {
@@ -74,28 +74,151 @@ public:
 
         while (currentNode) 
         {
-            // Return the node if the item name matches
+            // return the node if the item name matches
             if (currentNode->itemName.compare(desiredItem) == 0)
             {
-                cout << currentNode->itemName << ": quantity " 
-                    << currentNode->quantity <<", price: $" << currentNode->price << endl;
                 return currentNode;
             }
-            // Navigate to the left if the search item name is 
+            // navigate to the left if the search item name is 
             // less than the current node's item name
             else if (desiredItem.compare(currentNode->itemName) < 0) 
             {
                 currentNode = currentNode->left;
             }
 
-            // Navigate to the right if the search item name is 
+            // navigate to the right if the search item name is 
             // greater than the current node's item name.
             else 
             {
                 currentNode = currentNode->right;
             }
         }
-        cout << "Item not found!" << endl; // The item was not found in the tree
+        return nullptr; //the item was not found in the tree
     }//end of Search function
+
+    //Remove function to remove an item from binary tree
+    bool Remove(string itemName) 
+    {
+        //initalize parent pointer and current node
+        Node* parent = nullptr;
+        Node* currentNode = root;
+
+        // search for the item to be removed
+        while (currentNode) 
+        {
+            //if current node has matching item name
+            if (currentNode->itemName.compare(itemName) == 0)
+            {
+                //if current node is a leaf node
+                if (currentNode->left == nullptr && currentNode->right == nullptr) 
+                { 
+                    //if current node is root with no children 
+                    // then remove node if true
+                    if (parent == nullptr)
+                    { 
+                        root = nullptr;
+                    }
+                    //checks if current node is a left/right child leaf node
+                    //then set parent's left/right child pointer to null
+                    else if (parent->left == currentNode) 
+                    {
+                        parent->left = nullptr;
+                    } 
+                    else 
+                    {
+                        parent->right = nullptr;
+                    }
+                    delete currentNode; //deletes item from binary tree
+                    cout << "Item sucessfully deleted!" << endl;
+                    return true;
+                }
+                //if current node has a left child
+                else if (currentNode->left && currentNode->right == nullptr) 
+                {
+                    if (parent == nullptr) //node is root
+                    {
+                        root = currentNode->left; //set root as current node's left child
+                    }
+                    //if current node is not a root then sets
+                    //parent's left/right child pointer to current node's left child
+                    else if (parent->left == currentNode) 
+                    {
+                        parent->left = currentNode->left;
+                    }
+                    else 
+                    {
+                        parent->right = currentNode->left;
+                    }
+                    delete currentNode; //deletes item from binary tree
+                    cout << "Item sucessfully deleted!" << endl;
+                    return true;
+                }
+                //if current node has a right child
+                else if (currentNode->left == nullptr && currentNode->right) 
+                {
+                    if (parent == nullptr) //node is root
+                    { 
+                        root = currentNode->right;
+                    }
+                    //if current node is not a root then sets
+                    //parent's left/right child pointer to current node's right child
+                    else if (parent->left == currentNode) 
+                    {
+                        parent->left = currentNode->right;
+                    }
+                    else {
+                        parent->right = currentNode->right;
+                    }
+                    delete currentNode; //deletes item from binary tree
+                    cout << "Item sucessfully deleted!" << endl;
+                    return true;
+                }
+                //if current node has two children
+                else 
+                { 
+                    //find successor (leftmost child of right subtree)
+                    Node* successor = currentNode->right;
+                    while (successor->left) {
+                        successor = successor->left;
+                    }
+                    currentNode->itemName = successor->itemName; //copy successor's item name to current node
+                    parent = currentNode;
+
+                    //reassigned current node and item name so that loop continues with new item name
+                    currentNode = currentNode->right;
+                    itemName = successor->itemName;
+                }
+            } 
+            //if current node does not equal item name
+            else if (currentNode->itemName.compare(itemName) > 0) //search right
+            { 
+                parent = currentNode;
+                currentNode = currentNode->right;
+            }
+            else //search left
+            {
+                parent = currentNode;
+                currentNode = currentNode->left;
+            }
+        }
+        cout << "Item not found!" << endl; //node not found
+        return false;
+    } //end of Remove function
+
+    //Edit function to change item quanity and price
+    bool Edit(string itemName, int quantity, double price)
+    {
+        Node* item = Search(itemName); //searches for item first
+        if (item != nullptr)
+        {
+            //sets new quantity and price for item
+            item->quantity = quantity;
+            item->price = price;
+            cout << "Edit sucessful!" << endl;
+            return true;
+        }
+        cout << "Item not found!" << endl;
+        return false;
+    }
 };
 #endif
--- Semester Project Lab/semester-project-lab.cpp
+++ Semester Project Lab/semester-project-lab.cpp
@@ -7,13 +7,14 @@ Lab: Semester Project Lab
 
 Windows 11 Home, 24H2, ISO C++14 Standard
 
-Description: This program solves the problem of revtriving the 
-quanity and price of an item in a large list.
+Description: This program solves the problem of finding an item in storage 
+and allowing the user to edit or delete the item.
 */
 
 #include "BinarySearchTree.h"
 #include "Node.h"
 #include <iostream>
+#include <conio.h> 
 using namespace std;
 
 int main()
@@ -22,31 +23,74 @@ int main()
     BinarySearchTree tree;
     tree.Insert("apple", 3, .5);
     tree.Insert("banana", 1, 1.25);
+    tree.Insert("milk", 10, 5);
+    tree.Insert("watermelon", 20, 5.37);
+    tree.Insert("pizza", 6, 7.99);
+    tree.Insert("zucchini", 15, 4.32);
 
     //asks user if they want to either: 
-    //find an item, edit an items quanity, or edit an item's price
-    int userInput;
-    cout << "Type 1 to find an item | Type 2 to edit an item's quanity/price | Type 3 to delete an item" << endl;
-    cout << "Type Here: ";
-    cin >> userInput;
-    if (userInput == 1) //find item
-    {
-        //asks user to type what item to find
-        string item;
-        cout << "Input Item Name: ";
-        cin >> item;
-        //search for item in binary tree 
-        //and print out its quanity and price
-        tree.Search(item);
-    }
-    else if (userInput == 2) //edit item
-    {
+    //find an item, edit an item's quanity and price, delete item, or quit
+    int userInput = 0;
+    string item;
 
-    }
-    else //delete item
-    {
+    while (true)
+    { 
+        cout << "Type 1 to find an item | Type 2 to edit an item's quanity/price | Type 3 to delete an item | Type 4 to quit" << endl;
+        cout << "Type Here: ";
+        cin >> userInput;
 
+        if (userInput == 1) //find item
+        {
+            //asks user to type what item to find
+            cout << "Type item name to find: ";
+            cin >> item;
+
+            //search for item in binary tree 
+            //and print out its quanity and price
+            if (tree.Search(item) != nullptr)
+            {
+                Node* node = tree.Search(item);
+                cout << node->itemName << " | quantity: "
+                    << node->quantity << " | price | $" << node->price << endl;
+            }
+            else
+            {
+                cout << "Item not found!" << endl;
+            }
+
+            //clear screen after user presses a key
+            cout << "Press any key to continue...";
+            int ch = _getch();
+            cout << "\033[2J\033[1;1H";
+        }
+        else if (userInput == 2) //edit item
+        {
+            int quantity;
+            double price;
+            cout << "Type item name to edit: ";
+            cin >> item;
+            cout << "Type new quanity of " << item << ": ";
+            cin >> quantity;
+            cout << "Type new price of " << item << ": ";
+            cin >> price;
+            tree.Edit(item, quantity, price);
+
+            //clear screen after user presses a key
+            cout << "Press any key to continue...";
+            int ch = _getch();
+            cout << "\033[2J\033[1;1H";
+        }
+        else if (userInput == 3) //delete item
+        {
+            cout << "Type item name to delete: ";
+            cin >> item;
+            tree.Remove(item);
+
+            //clear screen after user presses a key
+            cout << "Press any key to continue...";
+            int ch = _getch();
+            cout << "\033[2J\033[1;1H";
+        }
     }
-    //allow adding and deleting quanity of items and editing price.
 }
 
