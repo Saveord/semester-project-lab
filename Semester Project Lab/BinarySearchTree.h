28d5733c3b2744a85172d8fbc9afae2b3489ca0f
Alexa Wu <porkson12@gmail.com>
2025-04-26 18:13:46 -0700

    fixed string comparision and added search function

--- Semester Project Lab/BinarySearchTree.h
+++ Semester Project Lab/BinarySearchTree.h
@@ -15,7 +15,7 @@ public:
 	{
 		root = nullptr;
 	}
-    //fuction to add new items to binary tree
+    //Insert fuction to add new items to binary tree
 	void Insert(string itemName, int quantity, double price)
 	{
         Node* item = new Node(itemName, quantity, price); //creates new node for item
@@ -30,9 +30,11 @@ public:
 
             while (currentNode) 
             {
-                if (item->itemName < currentNode->itemName) 
+                //Compares if item name is alphabetically less than current node's item name
+                if (item->itemName.compare(currentNode->itemName) < 0) 
                 {
-                    //If item is less than current node then assign it to the current node's left child
+                    //If item is less than current node then 
+                    //assign it to the current node's left child
                     if (currentNode->left == nullptr) 
                     {
                         currentNode->left = item;
@@ -45,7 +47,8 @@ public:
                         currentNode = currentNode->left;
                     }
                 }
-                //If item is greater than current node then assign it to the current node's right child
+                //If item is greater than current node then 
+                //assign it to the current node's right child
                 else 
                 {
                     if (currentNode->right == nullptr) 
@@ -63,5 +66,36 @@ public:
             }
         }
 	} //end of Insert function
+
+    //Search function to search for an item in the binary tree
+    Node* Search(string desiredItem) 
+    {
+        Node* currentNode = root;
+
+        while (currentNode) 
+        {
+            // Return the node if the item name matches
+            if (currentNode->itemName.compare(desiredItem) == 0)
+            {
+                cout << currentNode->itemName << ": quantity " 
+                    << currentNode->quantity <<", price: $" << currentNode->price << endl;
+                return currentNode;
+            }
+            // Navigate to the left if the search item name is 
+            // less than the current node's item name
+            else if (desiredItem.compare(currentNode->itemName) < 0) 
+            {
+                currentNode = currentNode->left;
+            }
+
+            // Navigate to the right if the search item name is 
+            // greater than the current node's item name.
+            else 
+            {
+                currentNode = currentNode->right;
+            }
+        }
+        cout << "Item not found!" << endl; // The item was not found in the tree
+    }//end of Search function
 };
 #endif
--- Semester Project Lab/semester-project-lab.cpp
+++ Semester Project Lab/semester-project-lab.cpp
@@ -1,6 +1,3 @@
-// Programmer: Alexa Wu
-// Programmer's ID: 1234567
-
 /*
 Alexa Wu
 
@@ -10,7 +7,8 @@ Lab: Semester Project Lab
 
 Windows 11 Home, 24H2, ISO C++14 Standard
 
-Description: This program solves the problem of revtriving the quanity and price of an item in a large list.
+Description: This program solves the problem of revtriving the 
+quanity and price of an item in a large list.
 */
 
 #include "BinarySearchTree.h"
@@ -20,15 +18,35 @@ using namespace std;
 
 int main()
 {
-    //add staring list of items currently in list
-    
-    //add user input for what item to find
-    string item;
-    cout << "Input Item Name: ";
-    cin >> item;
-    //use BST to search for item in list
+    //add staring binary tree of items
+    BinarySearchTree tree;
+    tree.Insert("apple", 3, .5);
+    tree.Insert("banana", 1, 1.25);
 
-    //print out quanity of item and price of item
+    //asks user if they want to either: 
+    //find an item, edit an items quanity, or edit an item's price
+    int userInput;
+    cout << "Type 1 to find an item | Type 2 to edit an item's quanity/price | Type 3 to delete an item" << endl;
+    cout << "Type Here: ";
+    cin >> userInput;
+    if (userInput == 1) //find item
+    {
+        //asks user to type what item to find
+        string item;
+        cout << "Input Item Name: ";
+        cin >> item;
+        //search for item in binary tree 
+        //and print out its quanity and price
+        tree.Search(item);
+    }
+    else if (userInput == 2) //edit item
+    {
+
+    }
+    else //delete item
+    {
+
+    }
     //allow adding and deleting quanity of items and editing price.
 }
 
